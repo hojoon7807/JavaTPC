faf9cc184e715ac49b09e937aa9afb3d66cdde20
hojoon <ujuj0202@gmail.com>
2022-01-12 21:34:12 +0900

    studyJava

---
+++ src/com/company/Study.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Study {
+
+    public static void main(String[] args) {
+	// write your code here
+        Study type = new Study();
+        type.checkByte();
+    }
+    public void checkByte(){
+        char charMin = 0;
+        System.out.println(true == true);
+    }
+}
